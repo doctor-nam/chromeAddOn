1bc9f8c99076c31a7e32ce446344e9e0db563666
남태진 <potatochip81@gmail.com>
2021-02-24 14:52:39 +0900

    210224 확장앱 first commit

---
+++ drnamAddOn/drnamAddOn.js
@@ -0,0 +1,152 @@
+$(function() {
+    addTargetBlank();
+    var url = window.location.href;
+
+    if (url.indexOf('kims') != -1) {
+        var $lastStyle = $('link[rel=Stylesheet]').last();
+        $('<link rel="Stylesheet" href="http://www.desitive.co.kr/kada/share/css/kada-custom.css">').insertAfter($('body'));
+    }
+
+    if (url.indexOf('youtube.com') != -1) {//유튜브
+        var repeat = setInterval(function() {
+            if ($('#buttons .btn-hide-bar').length == 0) {
+                $('#buttons').prepend('<a href="#none" class="btn-hide-bar normal">캡쳐 모드</a>');
+                console.log('yt');
+            }
+            else {
+                clearInterval(repeat);
+            }
+        }, 1000);
+
+		$(document).on('click', 'a.btn-hide-bar.normal', function() {
+            $(this).addClass('active');
+            $(this).removeClass('normal');
+            $(this).html('캡쳐 모드 해제');
+            $('.ytp-chrome-bottom, .ytp-gradient-bottom').hide();
+            return false;
+		});
+        $(document).on('click', 'a.btn-hide-bar.active', function() {
+            $(this).addClass('normal');
+            $(this).removeClass('active');
+            $(this).html('캡쳐 모드');
+            $('.ytp-chrome-bottom, .ytp-gradient-bottom').show();
+            return false;
+		});
+    }
+
+    if (url.indexOf('oksusu.com') != -1) {//옥수수
+        $('body').append('<a href="#none" class="btn-oksusu-zoom">+</a>');
+
+		$('a.btn-oksusu-zoom').on('click', function() {
+			$('body').toggleClass('oksusu-zoom');
+
+			if ($('body').hasClass('oksusu-zoom'))
+			{
+				$("html, body").stop().animate({scrollTop:120}, 500, 'swing');
+				$('a.btn-oksusu-zoom').html('-');
+			}
+			else {
+				$("html, body").stop().animate({scrollTop:0}, 500, 'swing');
+				$('a.btn-oksusu-zoom').html('+');
+			}
+		});
+    }
+
+    if (url.indexOf('http://lpcd.co.kr/') != -1) {
+        $('style + table').find('td').each(function() {
+            var src = $(this).find('.sell_price + img').attr('src');
+            console.log(typeof src);
+            if (typeof src != 'undefined') {
+                if (src.indexOf('icon_product0.gif') != -1) {
+                    $(this).css('opacity', '0.1');
+                }
+            }
+        });
+    }
+
+    if (url.indexOf('smartstore.naver.com') != -1) {
+        $('#content > div > ul > li').each(function() {
+            var html = $(this).find('.text').html();
+            if (typeof html != 'undefined') {
+                if (html.indexOf('품절') != -1) {
+                    $(this).css({'position':'fixed', 'top':'-9999px', 'left':'-9999px'});
+                }
+            }
+        });
+    }
+
+    if (url.indexOf('//ouo') != -1) {
+        $('button.btn-main').removeClass('disabled').css('z-index', 999999999999999999999999999);
+        $('.footer-copy').nextAll('div').eq(0).remove();
+    }
+
+    if (url.indexOf('//javfreefull') != -1) {
+        removeStreamElem();
+        $(document).on('DOMNodeInserted', function(e) {
+            console.log(typeof $(e.target).attr('id'));
+            if ( $(e.target).is('div') && $(e.target).children('a').attr('id').indexOf('container') != 0 ) {
+                $(e.target).remove();
+            }
+            if ( $("body").children(e.target).is('iframe')) {
+                $(e.target).remove();
+            }
+        });
+
+        $("body").append("<a href='#none' class='btn-mp4-play'>Play</a>");
+        $(document).on('click', 'a.btn-mp4-play', function() {
+            alert($('iframe').contents().find('a').length);
+        });
+
+    }
+
+    if (url.indexOf('dolpixel') != -1) {
+        $('#username').val('othertheside@naver.com');
+        $('#password').val('orange30');
+        $('body').addClass('dolpixel');
+        $('body.dolpixel .gallery-display > figure > a, body.dolpixel .imagesFromUser > a, body.dolpixel .more-from-site > a').prop('target', '_blank');
+    }
+
+    if (url.indexOf('winktv.co.kr') != -1) {
+        $('.layerPop-btnCancel > input:checkbox').prop('checked', true);
+        $('.layerPop-btnCancel').trigger('click');
+
+        $('#alert_layer').on('DOMNodeInserted', function(e) {
+            $('#user_id').val('goodl2u');
+            $('#user_pw').val('!@#wink4489');
+        });
+    }
+
+    if (url.indexOf('//sh.') != -1) {
+        // $('#timer').remove();
+        // $('.skip-btn').addClass('show');
+    }
+});
+
+$(window).on("load", function() {
+    addTargetBlank();
+});
+
+$(window).on("scroll", function() {
+    addTargetBlank();
+});
+
+function addTargetBlank() {
+    $('.post-body.entry-content a').each(function() {
+        $(this).attr('target', '_blank');
+    });
+}
+
+/* 새창 막기 함수 */
+function winOpen(url)
+{
+    //check if child window is already open
+    if (childWin &! childWin.closed && childWin.focus){
+        childWin.focus();
+    } else {
+        childWin = window.open(url,'','width=800,height=600');
+    }
+}
+
+/* stream */
+function removeStreamElem() {
+}
---
+++ drnamAddOn/twitterAddOn.js
@@ -0,0 +1,135 @@
+$(function() {
+    createDownload();
+    $('.nav.js-global-actions, .new-tweets-bar').mouseup(function() {
+        createDownload();
+    });
+
+    twitterIcoChange();
+    twitterTitleChange();
+
+    $("title").bind("DOMSubtreeModified", function(){
+        twitterTitleChange();
+    });
+
+    $('.people.notifications .new-count .count-inner').bind("DOMSubtreeModified", function(){
+        var cnt = $(this).html();
+        //twitterPush(cnt);
+    });
+
+    $('.PermalinkOverlay-modal, .stream-container').on('DOMNodeInserted', function(e) {
+        createDownload();
+    });
+
+    $('.AdaptiveMedia-photoContainer, div[aria-label=이미지]').on('mouseenter', function(e) {
+        createDownload();
+    });
+
+    $('.Gallery-media').on('DOMNodeInserted', function(e) {
+        $(this).siblings('.btn').remove();
+        createDownload();
+    });
+
+    $('.btn-img-download').on('DOMNodeInserted', function(e) {
+        alert($(this).attr('href'));
+    });
+
+    /* 60초에 한 번씩 알림 체크
+    setInterval(function() {
+        var cnt = $('.people.notifications .new-count .count-inner').html();
+        //twitterPush(cnt);
+    }, 60000); */
+});
+
+$(window).on("load", function() {
+    createDownload();
+    twitterIcoChange();
+    twitterTitleChange();
+
+    var cnt = $('.people.notifications .new-count .count-inner').html();
+    //twitterPush(cnt);
+});
+
+$(window).on("scroll", function() {
+    createDownload();
+    twitterIcoChange();
+    twitterTitleChange();
+
+    var cnt = $('.people.notifications .new-count .count-inner').html();
+    //twitterPush(cnt);
+});
+
+var rep = '다음';
+
+function createDownload() {
+    $('.AdaptiveMedia-photoContainer, .Gallery-media').each(function() {
+        var imgUrl = $(this).children('img').attr('src');
+        if (typeof imgUrl != 'undefined') {
+            var large = ':large'
+            if (imgUrl.indexOf(':large') != -1 || imgUrl.indexOf('profile_images') != -1) {
+                large = '';
+            }
+            if ($(this).siblings('.btn').length == 0) {
+                $(this).after("<a class='btn'></a>");
+                $(this).siblings('.btn').attr({'href' : imgUrl+large, 'target':'_blank'});
+            }
+        }
+    });
+
+    $('div[aria-label=이미지]').each(function() {
+        var imgUrl = $(this).children('img').attr('src');
+        if (typeof imgUrl != 'undefined') {
+            if ($(this).closest('a').find('.btn-img-download').length == 0) {
+                $(this).closest('a').append("<a class='btn-img-download'></a>");
+                $(this).children('.btn-img-download').attr({'href' : imgUrl, 'target':'_blank'});
+            }
+        }
+    });
+
+    /* gif 가져오기 담에 하자..
+    $('.PlayableMedia-reactWrapper').each(function() {
+        var vidUrl = $(this).find('video').attr('src');
+
+        if(typeof vidUrl != 'undefined') {
+            if(vidUrl.indexOf('blob:') != -1){
+                vidUrl = '';
+            }
+
+            if ($(this).nextAll('.btn').length == 0 && vidUrl.length != 0) {
+                $(this).siblings('.btn').remove();
+                $(this).after("<a class='btn'></a>");
+                $(this).siblings('.btn').prop({'href' : vidUrl, 'target':'_blank'});
+            }
+        }
+    }); */
+}
+
+function twitterIcoChange() {
+    var ico = chrome.extension.getURL('favicon-daum.ico');
+    $('link[rel="shortcut icon"], link[rel="mask-icon"]').attr('href', ico);
+}
+
+function twitterTitleChange() {
+    /*
+    if ($('title').html().indexOf('트위터') != -1) {
+        var tit = $('title').html().replace('트위터', rep);
+        //alert(tit);
+        $('title').html(tit);
+    }*/
+}
+
+function twitterPush(cnt) { // 알림 설정
+    var cnt = cnt;
+    //console.log(cnt);
+    if ($('title').html().indexOf('알림') == -1) {
+        var ti = $('title').html();
+        var tiSplit = ti.split('] '); // 문자열로 자르기
+        var tiFirst = ti.substring(0,1); // 문자열로 자르기
+
+        if (cnt != 0 && typeof cnt != 'undefined' && tiFirst != '[') {
+            $('title').html('['+cnt+'] '+ ti);
+        }
+        else {
+            $('title').html(tiSplit[1]);
+        }
+    }
+}
